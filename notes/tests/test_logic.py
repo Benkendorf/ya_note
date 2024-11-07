346fa32cf161e54f0e0bf5f287e2f08af19ba241
Egor Korotaev <ego_kor@mail.ru>
2024-11-07 12:55:10 +0300

    After Homework of Sprint 11/19 Topic 4/8 Lesson 5/5

--- notes/tests/test_logic.py
+++ notes/tests/test_logic.py
@@ -89,3 +89,80 @@ class TestSlugUniqueConstraint(TestCase):
         )
         notes_count = Note.objects.count()
         self.assertEqual(notes_count, 1)
+
+
+class TestNoteEditDelete(TestCase):
+    NOTE_TITLE = 'Заголовок заметки'
+    NOTE_TEXT = 'Текст заметки.'
+    NOTE_SLUG = 'test-slug'
+    ANOTHER_NOTE_SLUG = 'another-slug'
+
+    NEW_TITLE = 'Новый заголовок'
+    NEW_TEXT = 'Новый текст'
+    NEW_SLUG = 'new-slug'
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create(username='Мимо Крокодил')
+        cls.auth_client = Client()
+        cls.auth_client.force_login(cls.user)
+
+        cls.another_user = User.objects.create(username='Another')
+        cls.another_client = Client()
+        cls.another_client.force_login(cls.another_user)
+
+        cls.form_data = {
+            'title': cls.NEW_TITLE,
+            'text': cls.NEW_TEXT,
+            'slug': cls.NEW_SLUG
+        }
+        cls.note = Note.objects.create(
+            title=cls.NOTE_TITLE,
+            text=cls.NOTE_TEXT,
+            slug=cls.NOTE_SLUG,
+            author=cls.user
+        )
+
+        cls.another_note = Note.objects.create(
+            title=cls.NOTE_TITLE,
+            text=cls.NOTE_TEXT,
+            slug=cls.ANOTHER_NOTE_SLUG,
+            author=cls.user
+        )
+
+        cls.edit_url = reverse('notes:edit', args=(cls.note.slug,))
+        cls.delete_url = reverse('notes:delete', args=(cls.note.slug,))
+
+    def test_author_can_edit_own_note(self):
+        self.auth_client.post(self.edit_url, data=self.form_data)
+        self.note.refresh_from_db()
+        self.assertEqual(self.note.title, self.NEW_TITLE)
+        self.assertEqual(self.note.text, self.NEW_TEXT)
+        self.assertEqual(self.note.slug, self.NEW_SLUG)
+
+    def test_author_cant_edit_to_not_unique_slug(self):
+        self.form_data['slug'] = self.ANOTHER_NOTE_SLUG
+        response = self.auth_client.post(self.edit_url, data=self.form_data)
+        self.assertFormError(
+            response,
+            form='form',
+            field='slug',
+            errors=self.ANOTHER_NOTE_SLUG + WARNING
+        )
+
+    def test_author_can_delete_own_note(self):
+        self.auth_client.delete(self.delete_url)
+        notes_count = Note.objects.count()
+        self.assertEqual(notes_count, 1)
+
+    def test_user_cant_edit_note_of_another_user(self):
+        response = self.another_client.post(self.edit_url, data=self.form_data)
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+        self.note.refresh_from_db()
+        self.assertEqual(self.note.text, self.NOTE_TEXT)
+
+    def test_user_cant_delete_note_of_another_user(self):
+        response = self.another_client.delete(self.delete_url)
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+        notes_count = Note.objects.count()
+        self.assertEqual(notes_count, 2)
